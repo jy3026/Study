4ad96515b7dba59ef62571beb15763dcc1fdd7ec
jy3026 <jy_3026@naver.com>
2023-07-31 21:47:48 +0900

    feat: 회원 엔티티 구현

---
+++ src/main/java/wanted/jinyoung/member/entity/Member.java
@@ -0,0 +1,23 @@
+package wanted.jinyoung.member.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id",nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
