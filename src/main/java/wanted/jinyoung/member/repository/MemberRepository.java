ee5ac7c782f1108f51e13d2a550cd98acfba118a
jy3026 <jy_3026@naver.com>
2023-07-31 23:07:44 +0900

    feat: 멤버 레포지토리 구현

---
+++ src/main/java/wanted/jinyoung/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package wanted.jinyoung.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wanted.jinyoung.member.entity.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email);
+}
