716f311b74d7ccfe33fa106b9b3a98884677cf83
jy3026 <jy_3026@naver.com>
2023-07-31 21:07:42 +0900

    feat: 초기 셋팅

---
+++ src/main/java/wanted/jinyoung/JinyoungApplication.java
@@ -0,0 +1,13 @@
+package wanted.jinyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinyoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinyoungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wanted/jinyoung/JinyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.jinyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinyoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
