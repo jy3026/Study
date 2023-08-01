44605ab1090cbbbf98d30ad0baf62089bdef3be6
jy3026 <jy_3026@naver.com>
2023-08-01 22:47:44 +0900

    feat: 인증을 처리하는 로직 구현

---
+++ src/main/java/wanted/jinyoung/auth/userdetails/MemberDetailsService.java
@@ -0,0 +1,69 @@
+package wanted.jinyoung.auth.userdetails;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import wanted.jinyoung.auth.utills.CustomAuthorityUtils;
+import wanted.jinyoung.exception.BusinessLogicException;
+import wanted.jinyoung.exception.ExceptionCode;
+import wanted.jinyoung.member.entity.Member;
+import wanted.jinyoung.member.repository.MemberRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    private final CustomAuthorityUtils authorityUtils;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+        Member findMember = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return new MemberDetails(findMember);
+    }
+
+    private final class MemberDetails extends Member implements UserDetails {
+        MemberDetails(Member member) {
+            updateMemberInfo(member.getId(),member.getRoles());
+            updateEmail(member.getEmail());
+            updatePassword(member.getPassword());
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return authorityUtils.createAuthorities(this.getRoles());
+        }
+
+        @Override
+        public String getUsername() {
+            return getEmail();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+}
