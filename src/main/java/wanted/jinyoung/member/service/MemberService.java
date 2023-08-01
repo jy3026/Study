01d6f05e8ba2d8ed0fd18fecf610260676cf1d83
jy3026 <jy_3026@naver.com>
2023-08-01 23:02:47 +0900

    fix: 회원 가입시 룰을 DB에 저장 하도록 변경

--- src/main/java/wanted/jinyoung/member/service/MemberService.java
+++ src/main/java/wanted/jinyoung/member/service/MemberService.java
@@ -3,12 +3,14 @@ package wanted.jinyoung.member.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import wanted.jinyoung.auth.utills.CustomAuthorityUtils;
 import wanted.jinyoung.exception.BusinessLogicException;
 import wanted.jinyoung.exception.ExceptionCode;
 import wanted.jinyoung.member.entity.Member;
 import wanted.jinyoung.member.repository.MemberRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,8 +19,12 @@ import java.util.Optional;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final CustomAuthorityUtils authorityUtils;
     public void createMember(Member member){
         verifyExistsEmail(member.getEmail());
+
+        List<String> roles = authorityUtils.createRoles(member.getEmail());
+        member.updateRoles(roles);
 
         String encryptedPassword = passwordEncoder.encode(member.getPassword());
         member.updatePassword(encryptedPassword);
