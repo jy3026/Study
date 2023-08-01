cc7c4f60569f4e4e2dfccbe508e6b39f66f0af09
jy3026 <jy_3026@naver.com>
2023-08-01 23:09:21 +0900

    feat: 비즈니스 예외 추가

--- src/main/java/wanted/jinyoung/exception/ExceptionCode.java
+++ src/main/java/wanted/jinyoung/exception/ExceptionCode.java
@@ -3,7 +3,10 @@ package wanted.jinyoung.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-    MEMBER_EXISTS(409, "Member exists");
+    MEMBER_EXISTS(409, "Member exists"),
+    MEMBER_NOT_FOUND(404, "Member not found"),
+    NOT_VALID_EMAIL_FORMAT(401,"유효 하지 않은 이메일 형식 입니다"),
+    ENTER_AT_LEAST_EIGHT(401,"비밀번호를 8자리 이상 입력하세요");
 
     @Getter
     private int status;
