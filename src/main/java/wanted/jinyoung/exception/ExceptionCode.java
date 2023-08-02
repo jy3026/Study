053e2c058c793f35ca7632ecc79f9e2b3c0951f2
jy3026 <jy_3026@naver.com>
2023-08-02 22:26:01 +0900

    feat: 익셉션코드 추가

--- src/main/java/wanted/jinyoung/exception/ExceptionCode.java
+++ src/main/java/wanted/jinyoung/exception/ExceptionCode.java
@@ -6,7 +6,8 @@ public enum ExceptionCode {
     MEMBER_EXISTS(409, "Member exists"),
     MEMBER_NOT_FOUND(404, "Member not found"),
     NOT_VALID_EMAIL_FORMAT(401,"유효 하지 않은 이메일 형식 입니다"),
-    ENTER_AT_LEAST_EIGHT(401,"비밀번호를 8자리 이상 입력하세요");
+    ENTER_AT_LEAST_EIGHT(401,"비밀번호를 8자리 이상 입력하세요"),
+    BOARD_NOT_FOUND(404,"Board not found");
 
     @Getter
     private int status;
