16860ebb3f2fe63313f952b7b904b97669a1b2e7
jy3026 <jy_3026@naver.com>
2023-08-03 23:20:08 +0900

    feat: Patch변환 매퍼 추가

--- src/main/java/wanted/jinyoung/board/mapper/BoardMapper.java
+++ src/main/java/wanted/jinyoung/board/mapper/BoardMapper.java
@@ -5,9 +5,15 @@ import org.mapstruct.ReportingPolicy;
 import wanted.jinyoung.board.dto.BoardDto;
 import wanted.jinyoung.board.entity.Board;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BoardMapper {
     Board boardPostToBoard(BoardDto.Post requestBody);
 
     BoardDto.Response boardToBoardResponse(Board board);
+
+    List<BoardDto.Response> boardsToBoardResponses(List<Board> boards);
+
+    Board boardPatchToBoard(BoardDto.Patch requestBody);
 }
