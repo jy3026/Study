3ba2af290a283b3666d8cd133fdabd11d79c7bf2
jy3026 <jy_3026@naver.com>
2023-08-03 23:21:31 +0900

    feat: 조회,수정,삭제 서비스 로직 구현

--- src/main/java/wanted/jinyoung/board/service/BoardService.java
+++ src/main/java/wanted/jinyoung/board/service/BoardService.java
@@ -1,6 +1,9 @@
 package wanted.jinyoung.board.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import wanted.jinyoung.board.entity.Board;
@@ -30,9 +33,41 @@ public class BoardService {
         return optionalBoard.orElseThrow(() -> new BusinessLogicException(ExceptionCode.BOARD_NOT_FOUND));
     }
 
+    public Page<Board> findBoards(int page,int size){
+        return boardRepository.findAll(PageRequest.of(page-1, size, Sort.by("id").descending()));
+    }
+
+    public Board updateBoard(Board board){
+        Optional<Board> optionalBoard = boardRepository.findById(board.getId());
+        Board findBoard = optionalBoard.orElseThrow(() -> new BusinessLogicException(ExceptionCode.BOARD_NOT_FOUND));
+
+        checkAuthor(findLoggedInMember().getId(),findBoard.getMember().getId());
+
+        Optional.ofNullable(board.getTitle()).ifPresent(t -> findBoard.setTitle(t));
+        Optional.ofNullable(board.getContents()).ifPresent(c -> findBoard.setContents(c));
+
+        return boardRepository.save(findBoard);
+    }
+
+    public void removeBoard(long boardId){
+        Optional<Board> optionalBoard = boardRepository.findById(boardId);
+        Board findBoard = optionalBoard.orElseThrow(() -> new BusinessLogicException(ExceptionCode.BOARD_NOT_FOUND));
+
+        checkAuthor(findLoggedInMember().getId(),findBoard.getMember().getId());
+
+        boardRepository.delete(findBoard);
+    }
+
     private Member findLoggedInMember() {
         String principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
         Optional<Member> findbyEmailMember = memberRepository.findByEmail(principal);
-        return findbyEmailMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_EXISTS));
+
+        return findbyEmailMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.CHECK_LOGIN));
+    }
+
+    private static void checkAuthor(long memberId, long authorId) {
+        if (authorId != memberId) {
+            throw new BusinessLogicException(ExceptionCode.ONLY_AUTHOR);
+        }
     }
 }
